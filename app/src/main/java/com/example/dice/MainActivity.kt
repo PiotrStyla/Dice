479b599b93818eeebb2d6e6a33caf3391eb7907b
Admin <p.styla@gmail.com>
2019-06-03 10:51:04 +0200

    1

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.dice
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's roll"
+        rollButton.setOnClickListener {
+            rollDice()
+
+        }
+        
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice rolled!"
     }
 }
